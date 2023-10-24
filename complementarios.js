2732030075f199d8a1e98e201d5734d6312c0347
nosient0 <pachecoignaciodev@gmail.com>
2023-10-24 17:27:38 -0300

    Complementarios

---
+++ complementarios.js
@@ -0,0 +1,41 @@
+/* EJERCICIOS COMPLEMENTARIOS CODERHOUSE */
+
+    /* Actividad 1 , Homero */
+
+/* 
+let nombre = " Homero";
+
+let apellido = " Simpson ";
+
+let edad = 31;
+
+let identidadHomero = nombre + apellido + edad ;
+
+console.log(identidadHomero); 
+*/
+
+    /* Actividad 2, Ciudades */
+
+/* 
+const CIUDAD1 = "New York NY";
+const CIUDAD2 = "Buenos Aires BS";
+const CIUDAD3 = "San Francisco CA";
+const CIUDAD4 = "Bogota BOG";
+const CIUDAD5 = "Las Vegas NV";
+
+let ciudades = CIUDAD1 + " , " + CIUDAD2 + " , " + CIUDAD3 + " , " + CIUDAD4 + " , " + CIUDAD5;
+
+console.log(ciudades); 
+*/
+
+    /* Actividad 3, Carnet */
+
+/* 
+let nombre = " Bart ";
+let nacimiento = " 02-11-70 ";
+let direccion = " 742 Evergreen Terr. Sprinfield, USA ";
+
+let carnet = nombre + nacimiento + direccion;
+
+console.log(carnet); 
+*/
