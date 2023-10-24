d307fea564c89a0db2e3076f22ab4df189125a59
nosient0 <pachecoignaciodev@gmail.com>
2023-10-24 18:05:04 -0300

    Actividad 1

---
+++ actividades.js
@@ -0,0 +1,25 @@
+    /* Actividad 1 */
+
+/* EJEMPLO 1
+
+let nombre= prompt("Ingresa tu nombre");
+console.log("tu nombre es " + nombre) 
+*/
+
+/* EJEMPLO 2
+let nombre = prompt(" Ingre tu nombre");
+
+const mensaje = " Tu nombre es ";
+
+alert(mensaje + nombre); 
+*/
+
+/* EJEMPLO 3
+let nombre = prompt(" Ingre tu nombre ");
+let edad = prompt(" Ingresa tu edad ");
+
+const mensajeNombre = (" Tu nombre es ");
+const mensajeEdad = (" tu edad es de ");
+
+alert( mensajeNombre + nombre + mensajeEdad + edad + " años"); 
+*/
